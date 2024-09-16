03bb60c5c08be0c2e5c77ee47cf51bbeb356612d
Alessandro Serra <a.serra132@studenti.unica.it>
2024-09-16 14:58:30 +0200

    Silicon Bands Calculation

---
+++ Si-LAMMPS/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for iter in $(seq 1 1); do
+
+  echo -e "Starting iteration #${iter}...\n"
+
+  mpirun -np 10 lmp_mpi -in in.forces -var iter ${iter} | tee out.forces-${iter}
+
+done
+
+echo -e "\nDONE!\n"
